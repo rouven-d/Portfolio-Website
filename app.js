c7c59a481bf65206f8ec480807b235168226bd7e
Rouven Daunke <rouven.daunke@googlemail.com>
2021-07-03 13:38:45 +0200

    added all sections and content. Desktop styling complete.

--- app.js
+++ app.js
@@ -7,6 +7,14 @@ app.get("/", (req, res) => {
   res.sendFile(__dirname + "/index.html");
 });
 
+app.get("/legaldisclosure", (req, res) => {
+  res.sendFile(__dirname + "/legaldisclosure.html");
+});
+
+app.get("/privacypolicy", (req, res) => {
+  res.sendFile(__dirname + "/privacypolicy.html");
+});
+
 app.listen(3000, () => {
   console.log("server successfully started on Port 3000");
 });
