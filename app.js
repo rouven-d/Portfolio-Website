1f940221b81abce988d46fc733a02b4c3e56cb51
Rouven Daunke <rouven.daunke@googlemail.com>
2021-07-08 11:36:11 +0200

    Heroku Deploy preparations

--- app.js
+++ app.js
@@ -15,6 +15,8 @@ app.get("/privacypolicy", (req, res) => {
   res.sendFile(__dirname + "/privacypolicy.html");
 });
 
-app.listen(3000, () => {
-  console.log("server successfully started on Port 3000");
-});
+let port = process.env.PORT;
+if (port == null || port == "") {
+  port = 8000;
+}
+app.listen(port);
